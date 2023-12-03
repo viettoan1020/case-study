07b5d644c13e6e6b5b91b20989e773541747ba76
viettoan1020 <viettoann102003@gmail.com>
2023-12-03 22:37:33 +0700

    Add

--- web_ban_hang.js
+++ web_ban_hang.js
@@ -1,18 +1,33 @@
 let products = [
     {
         "title": " Nike Jordan 1 OG Low x Travis Scott Reverse Mocha (DM7866-162) ",
-        "text": "Condition:\n New with box: A brand-new, unused, and unworn item (including handmade items) in the original packaging (such as the original box or bag) and/or with the original tags attached.",
+        "text": "Condition:\n New",
         "image": "https://i.ebayimg.com/images/g/56MAAOSw4wBi4wX0/s-l1600.png",
-        "price": "$1,399",
-        "size": "4-14"
+        "price": "Price: $1,399",
+        "size": "Size: 4-14"
     },
     {
-        "title": " Nike Jordan 1 OG Low x Travis Scott Reverse Mocha (DM7866-162) ",
-        "text": "Condition:\n New with box: A brand-new, unused, and unworn item (including handmade items) in the original packaging (such as the original box or bag) and/or with the original tags attached.",
-        "image": "https://i.ebayimg.com/images/g/56MAAOSw4wBi4wX0/s-l1600.png",
-        "price": "$1,399",
-        "size": "4-14"
+        "title": " Nike Fragment Design x Travis Scott x Air Jordan 1 Retro Low DM7866-140 ",
+        "text": "Condition:\n New",
+        "image": "https://authentic-shoes.com/wp-content/uploads/2023/04/travis-scott-fragment-air-jordan-1-low-lateral-side_e107a77f3f714fbea604b17bd68427b1.png",
+        "price": "Price: $2,299",
+        "size": "Size: 4-14"
+    },
+    {
+        "title": " Adidas Yeezy Slide Foam RNNR MXT Moon Grey GV7904 ",
+        "text": "Condition:\n New",
+        "image": "https://authentic-shoes.com/wp-content/uploads/2023/04/adidas-yeezy-foam-rnnr-mxt-moon_45fe093b796547a8b885c9a8f0944089-768x330.png",
+        "price": "Price: $600",
+        "size": "Price: 4-13"
+    },
+    {
+        "title": " Adidas BBC Ice Cream x Pharrell x NMD Human Race ‘Astronaut’ GW3955 ",
+        "text": "Condition:\n New",
+        "image": "https://authentic-shoes.com/wp-content/uploads/2023/04/pharrell_williams_hu_nmd_shoes_g_d857caf2dfc8476789fe09429f381878-768x768.png",
+        "price": "Price: $199",
+        "size": "Price: 4-13"
     }
+
 ]
 document.addEventListener("DOMContentLoaded", function () {
         console.log(products)
@@ -64,5 +79,37 @@ btnAdd.addEventListener('click', function (event) {
     products.push(newProduct);
     renderData(products);
 });
+function renderData(products1) {
+    var listElement = ``;
+    const containerCard = document.getElementById("container_card");
+
+    products1.forEach((product, index) => {
+            var elementString = `
+              <div class="card" id="card">
+                <img src=${product.image} class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">${product.text}
+                            <br>
+                            ${product.price};
+                            <br>
+                            ${product.size};
+                        </p>
+                        <a href="#" class="btn btn-primary">Add To Cart</a>
+                        <button type="button" class="btn btn-danger" onclick="btnDelete(${index}) ">Delete Product</button>
+                    </div>
+                </div>    
+                `;
+            listElement += elementString;
+        }
+    )
+    containerCard.innerHTML = listElement;
+    function btnDelete(index) {
+        products.slice(index, 1)
+        renderData(products);
+    }
+}
+
+
 
 
